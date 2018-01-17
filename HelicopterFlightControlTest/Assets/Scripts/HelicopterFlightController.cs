78a8829830dd22433b13274c9357a2afa1371f70
Simple <simple3068@naver.com>
2018-01-17 18:06:26 +0900

    Upload Helicopter Flight Control Test
    - Force based Flight Control Test
      * LShift to Start/Kill the Engine
      * W/S to control Collective
      * A/D to control Anti-Torque
      * U/J/H/K to control Cycle
    - There  is no key to control Throttle, should change it later.

---
+++ HelicopterFlightControlTest/Assets/Scripts/HelicopterFlightController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterFlightController : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    Vector3 v3UpForce;
+    bool bEngineStatus = false;
+    float fEngineCoefficient = 0.0f;
+
+    float fCollective;
+    float fAntiTorque;
+    Vector3 v3CycleDir = Vector3.zero;
+
+    const float PASSIVE_INPUT_STEP = 0.03125f;
+    
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        v3UpForce = -Physics.gravity * rigidBody.mass;
+	}
+	
+	void FixedUpdate ()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            ToggleEngine();
+
+        if (bEngineStatus == false)
+        {
+
+        }
+        else
+        {
+            ControlCollective();
+            ControlAntiTorque();
+            ControlCycle();
+        }
+
+        rigidBody.AddRelativeForce((v3UpForce + v3CycleDir) * fEngineCoefficient);
+    }
+
+    void ToggleEngine()
+    {
+        if (bEngineStatus == false)
+        {
+            while (fEngineCoefficient < 1.0f)
+            {
+                fEngineCoefficient += 0.0015625f;
+            }
+        }
+        else
+        {
+            while (fEngineCoefficient > 0.0f)
+            {
+                fEngineCoefficient -= 0.0015625f;
+            }
+        }
+
+        bEngineStatus = !bEngineStatus;
+    }
+
+    void ControlCollective()
+    {
+        fCollective = Input.GetAxis("Vertical");
+
+        rigidBody.velocity = Vector3.Lerp(rigidBody.velocity, rigidBody.transform.up * fCollective, 0.0625f);
+    }
+
+    void ControlAntiTorque()
+    {
+        fAntiTorque = Input.GetAxis("Horizontal");
+
+        rigidBody.angularVelocity = Vector3.Lerp(rigidBody.angularVelocity, rigidBody.transform.up * fAntiTorque, 0.0625f);
+    }
+
+    void ControlCycle()
+    {
+        // U for Pitch Down
+        if (Input.GetKey(KeyCode.U))
+        {
+            if (v3CycleDir.z < 1.0f) v3CycleDir.z += PASSIVE_INPUT_STEP;
+        }
+        else
+        {
+            if (v3CycleDir.z > 0.0f) v3CycleDir.z -= PASSIVE_INPUT_STEP;
+        }
+
+        // J for Pitch Up
+        if (Input.GetKey(KeyCode.J))
+        {
+            if (v3CycleDir.z > -1.0f) v3CycleDir.z -= PASSIVE_INPUT_STEP;
+        }
+        else
+        {
+            if (v3CycleDir.z < 0.0f) v3CycleDir.z += PASSIVE_INPUT_STEP;
+        }
+
+        // K for Roll Right
+        if (Input.GetKey(KeyCode.K))
+        {
+            if (v3CycleDir.x < 1.0f) v3CycleDir.x += PASSIVE_INPUT_STEP;
+        }
+        else
+        {
+            if (v3CycleDir.x > 0.0f) v3CycleDir.x -= PASSIVE_INPUT_STEP;
+        }
+
+        // H for Roll Left
+        if (Input.GetKey(KeyCode.H))
+        {
+            if (v3CycleDir.x > -1.0f) v3CycleDir.x -= PASSIVE_INPUT_STEP;
+        }
+        else
+        {
+            if (v3CycleDir.x < 0.0f) v3CycleDir.x += PASSIVE_INPUT_STEP;
+        }
+
+        // Pitch
+        rigidBody.angularVelocity = Vector3.Lerp(rigidBody.angularVelocity, rigidBody.transform.right * v3CycleDir.z, 0.0625f);
+
+        rigidBody.angularVelocity = Vector3.Lerp(rigidBody.angularVelocity, -rigidBody.transform.forward * v3CycleDir.x, 0.0625f);
+    }
+}
