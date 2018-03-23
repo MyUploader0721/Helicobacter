d6b1df5822af13b2e8cd677c55595064a78c3af5
Simple <simple3068@naver.com>
2018-03-24 02:11:13 +0900

    Copy and Edit Demo Project
    * 최종 데모프로젝트를 복사하였습니다.
    * 조종 방식을 변경하였습니다.
      - 컬렉티브는 원래 속도를 조절하여 고도 변경을 하였습니다.
      - 기체가 90도로 기울어졌는데도 낙하하지 않는 오류가 있었습니다.
      - 따라서 속도 기반의 컬렉티브를 힘 기반 컬렉티브로 변경하였습니다.
      - 보다 사실적인 조종이 가능합니다.

--- HAVIO Flight Demo/Assets/Scripts/HelicopterSFXController.cs
+++ HAVIO Flight Demo/Assets/Scripts/HelicopterSFXController.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* =================================================================
+   Editor      : 곽수환
+   Date        : 2018. 2. 11.
+   Description : 헬리콥터의 전체적인 효과음을 담당하는 스크립트
+   Edit Log    : 
+    - 사용법 : 
+     * PlayStartEngine()과 PlayKillEngine()는
+       HelicopterFlightController의 ToggleEngine()에서밖에 사용되지
+       않습니다. 
+   ================================================================= */
+
 public class HelicopterSFXController : MonoBehaviour
 {
     AudioSource[] audioSource;
@@ -44,13 +55,20 @@ public class HelicopterSFXController : MonoBehaviour
             audioSource[1].volume = 0.0f;
     }
 
+    /// <summary>
+    /// 엔진 시동을 켤 때 엔진과 날개 회전 소리가 점점 빨라집니다. 
+    /// HelicopterFlightController의 ToggleEngine()에서 사용됩니다. 
+    /// </summary>
     public void PlayStartEngine()
     {
         StopAllCoroutines();
-        StartCoroutine(EngineStart());
+        StartCoroutine(EngineStartCoroutine());
     }
-
-    IEnumerator EngineStart()
+    /// <summary>
+    /// 엔진 시동을 켤 때 해당 사운드의 pitch를 점점 증가시킵니다. 
+    /// PlayStartEngine()에서 사용됩니다. 
+    /// </summary>
+    IEnumerator EngineStartCoroutine()
     {
         while (audioSource[0].pitch < 1.0f)
         {
@@ -61,13 +79,20 @@ public class HelicopterSFXController : MonoBehaviour
         bEngineStart = true;
     }
 
+    /// <summary>
+    /// 엔진 시동을 끌 때 엔진과 날개 회전 소리가 점점 느려집니다. 
+    /// HelicopterFlightController의 ToggleEngine()에서 사용됩니다. 
+    /// </summary>
     public void PlayKillEngine()
     {
         StopAllCoroutines();
-        StartCoroutine(EngineKill());
+        StartCoroutine(EngineKillCoroutine());
     }
-
-    IEnumerator EngineKill()
+    /// <summary>
+    /// 엔진 시동을 끌 때 해당 사운드의 pitch를 점점 감소시킵니다. 
+    /// PlayKillEngine()에서 사용됩니다. 
+    /// </summary>
+    IEnumerator EngineKillCoroutine()
     {
         bEngineStart = false;
 
