d60c165491e4ebd1051f2fc413829f1fdef7075a
Simple <simple3068@naver.com>
2018-05-02 12:01:08 +0900

    MAC모드 타겟 AI 수정 2
    * 타겟 오브젝트의 목적지 y값이 0보다 커져서 타겟이 제대로 움직이지 않는 오류 발생
      - y를 0으로 설정하여 일단 해결된 것 같음!

--- Project HAVIO/Assets/Scripts/GameModes/MidAirChaser/MACTargetBehaviour.cs
+++ Project HAVIO/Assets/Scripts/GameModes/MidAirChaser/MACTargetBehaviour.cs
@@ -13,6 +13,8 @@ using UnityEngine.AI;
  *  - NavMesh를 사용하여 주어진 위치로 이동합니다. 
  *  - 04-30: 타겟의 AI를 조금 변형합니다. 일정 범위 밖에서는 랜덤으로 이동하다가
  *           플레이어가 일정 범위 안으로 이동하면 플레이어와 반대 방향으로 이동하게 됩니다. 
+ *  - 05-02: 플레이어는 공중에 있으므로 타겟 바로 위에 위치하면 플레이어가 아주 조금씩 움직이거나
+ *           움직이질 못하는 것 같습니다. destination의 y를 0으로 설정하여 해결했습니다. 
  */
 
 public class MACTargetBehaviour : MonoBehaviour
@@ -49,6 +51,8 @@ public class MACTargetBehaviour : MonoBehaviour
         {
             bIsEscapingMode = true;
             Vector3 delta = transform.position - objPlayer.transform.position;
+            // EDITLOG: 05-02, 타겟의 종착지의 y값을 0으로 설정하여 지상에서 움직이도록 설정
+            delta.y = 0.0f;
             navMeshAgent.destination = transform.position + delta * delta.magnitude;
         }
 	}
