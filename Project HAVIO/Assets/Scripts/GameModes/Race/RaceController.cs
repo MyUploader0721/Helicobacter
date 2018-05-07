83a5f15647c49522e6f7c030c0676335aaaa5eb7
Simple <simple3068@naver.com>
2018-05-08 00:20:55 +0900

    Race 모드 프로토타입 개발
    * 팀원들은 이 모드의 존재를 모르는 상태입니다.
      - 눈썰미가 있는, 깃허브 자주 들어오는 팀원은 눈치를 채겠지요?
    * 일반적인 레이싱 게임
      - 주어진 시간 안에 모든 중간 통과지점을 통과합니다
      - 통과지점을 모두 통과하면 골인지점을 통과합니다
      - 시간초는 계속 줄어들지만, 중간 통과지점을 통과하면 시간이 추가됩니다
    * 주석 필요!
      - 아직 함수에 주석을 달지 못했습니다.
      - 꼭 달아야 하는데 지금은 너무 피곤해서 나중에 하겠습니다

---
+++ Project HAVIO/Assets/Scripts/GameModes/Race/RaceController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *       TITLE: RaceController.cs
+ *      AUTHOR: 곽수환
+ *        DATE: 2018-05-07
+ * DESCRIPTION: Race 게임모드를 컨트롤하는 스크립트
+ *     DEV LOG: 
+ *  - 정해진 지점을 통과하며 시간 내에 목표까지 도착해야 하는 레이싱 모드입니다. 
+ *  - 이 모드를 컨트롤하는 스크립트입니다.  
+ */
+
+public class RaceController : MonoBehaviour
+{
+    HelicopterInfo helicopterInfo;
+
+    [Header("Player Helicopter")]
+    [SerializeField] GameObject objPlayer;
+
+    [Header("Helicopter Setting")]
+    [SerializeField] bool bIsPlayWithGamePad = false;
+    [SerializeField] bool bIsStartInMidAir = false;
+
+    [Header("Helicopter Armament Setting(Recommends default)")]
+    [SerializeField] bool bUseSearchLight = true;
+    [SerializeField] bool bUseInnerPod = false;
+    [SerializeField] bool bUseOuterPod = false;
+
+    [Header("Game Mode Setting")]
+    [SerializeField] GameObject objTargetNav;
+    public bool bAccomplished = false;
+    [SerializeField] RacePassageBehaviour[] rpbPassages;
+    [SerializeField] GameObject objGoal;
+    public int nPassedPassages = 0;
+    public int nTimeRemained = 60;
+    public int nAmountOfTimeAddition = 5;
+    public bool bCanGoal = false;
+    public bool bHasGoalIn = false;
+
+    void Start()
+    {
+        if (objPlayer == null)
+            objPlayer = GameObject.FindGameObjectWithTag("Player");
+
+        helicopterInfo = objPlayer.GetComponent<HelicopterInfo>();
+        helicopterInfo.bIsPlayWithGamePad = bIsPlayWithGamePad;
+        helicopterInfo.bUseInnerPod = bUseInnerPod;
+        helicopterInfo.bUseOuterPod = bUseOuterPod;
+
+        for (int i = 0; i < rpbPassages.Length; i++)
+        {
+            rpbPassages[i].SetNumber(i);
+        }
+
+        StartCoroutine("StartTimer");
+    }
+
+    void Update()
+    {
+        if (bIsStartInMidAir)
+        {
+            bIsStartInMidAir = false;
+            objPlayer.GetComponent<InputController>().ToggleEngine();
+        }
+
+        if (nPassedPassages < rpbPassages.Length)
+        {
+            Vector3 v3PosNav = rpbPassages[nPassedPassages].transform.position;
+            v3PosNav.y += 30.0f;
+            objTargetNav.transform.position = v3PosNav;
+        }
+        else
+        {
+            Vector3 v3PosNav = objGoal.transform.position;
+            v3PosNav.y += 30.0f;
+            objTargetNav.transform.position = v3PosNav;
+        }
+    }
+
+    public void EnterPassage(int nNumber)
+    {
+        rpbPassages[nNumber] = null;
+
+        nPassedPassages++;
+        nTimeRemained += nAmountOfTimeAddition;
+
+        if (nNumber == rpbPassages.Length - 1)
+        {
+            bCanGoal = true;
+        }
+            
+    }
+
+    IEnumerator StartTimer()
+    {
+        while (nTimeRemained > 0)
+        {
+            nTimeRemained--;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
---
+++ Project HAVIO/Assets/Scripts/GameModes/Race/RacePassageBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *       TITLE: RacePassageBehaviour.cs
+ *      AUTHOR: 곽수환
+ *        DATE: 2018-05-07
+ * DESCRIPTION: 중간 통과지점의 행동을 정의하는 스크립트
+ *     DEV LOG: 
+ *  - 플레이어는 이 중간 통과지점을 순서대로 통과해야 합니다. 
+ */
+
+public class RacePassageBehaviour : MonoBehaviour
+{
+    [Header("Race Controller")]
+    [SerializeField] RaceController raceController;
+    [Header("Number of this Passage")]
+    [SerializeField] int nNumber = -1;
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && raceController.nPassedPassages == nNumber)
+        {
+            raceController.EnterPassage(nNumber);
+
+            Destroy(transform.gameObject);
+        }
+    }
+
+    public void SetNumber(int n)
+    {
+        if (nNumber == -1)
+        {
+            nNumber = n;
+        }
+    }
+}
