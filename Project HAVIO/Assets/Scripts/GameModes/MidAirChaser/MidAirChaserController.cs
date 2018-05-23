69874db792b2c3a4b300d772a5512682083d9aea
Simple <simple3068@naver.com>
2018-05-23 12:14:02 +0900

    MAC모드 임무 로직 수정
    * (minor한 스크립트 수정, 하지만 결과는 major합니다)
    * 플레이어가 타겟에 가까워지면 그 때서부터 임무가 시작됩니다.
    * 임무가 실패/성공하고 나서도 3초 카운트다운이 뜨지 않도록 합니다.
    * 주석을 조금 달았습니다.

--- Project HAVIO/Assets/Scripts/GameModes/MidAirChaser/MidAirChaserController.cs
+++ Project HAVIO/Assets/Scripts/GameModes/MidAirChaser/MidAirChaserController.cs
@@ -59,34 +59,44 @@ public class MidAirChaserController : MonoBehaviour
 
         helicopterInfo = objPlayer.GetComponent<HelicopterInfo>();
         helicopterInfo.bIsPlayWithGamePad = bIsPlayWithGamePad;
-        helicopterInfo.bUseInnerPod = bUseInnerPod;
-        helicopterInfo.bUseOuterPod = bUseOuterPod;
-        helicopterInfo.bUseSearchLight = bUseSearchLight;
+        helicopterInfo.bUseInnerPod       = bUseInnerPod;
+        helicopterInfo.bUseOuterPod       = bUseOuterPod;
+        helicopterInfo.bUseSearchLight    = bUseSearchLight;
 
         targetBehaviour = objTarget.GetComponent<MACTargetBehaviour>();
 
         nRemainedTime = nMissionTime;
-
-        StartCoroutine("MissionTimer");
+        textTime.text = "Time: " + nRemainedTime;
     }
 	
 	void Update ()
     {
-        textDistance.text = "Dist.: " + targetBehaviour.fDistance.ToString("0.00") + "m";
+        // 플레이어가 공중에서 시작할 경우
         if (bIsStartInMidAir)
         {
             bIsStartInMidAir = false;
             objPlayer.GetComponent<InputController>().ToggleEngine();
         }
 
+        // 05-21: 추격을 시작합니다. 
+        if (!bChaseStart && Vector3.Distance(targetBehaviour.transform.position, objPlayer.transform.position) <= fMaxDistance)
+        {
+            bChaseStart = true;
+            StartCoroutine("MissionTimer");
+        }
+
+        textDistance.text = "Dist.: " + targetBehaviour.fDistance.ToString("0.00") + "m";
+
+        // 목표와의 거리가 멀어질 경우
         if (bChaseStart && targetBehaviour.fDistance > fMaxDistance)
         {
-            if (!bMissingAlert)
+            if (!bMissingAlert && !bAccomplished && !bIsGameOver)
                 StartCoroutine("MissingCountdown");
 
             textDistance.color = new Color(0.8046875f, 0.0f, 0.0f);
             Debug.Log("Missing your target!: " + targetBehaviour.fDistance + "m");
         }
+        // 목표와의 거리가 가까울 경우
         else if (bChaseStart && targetBehaviour.fDistance <= fMaxDistance)
         {
             if (bMissingAlert)
@@ -100,13 +110,15 @@ public class MidAirChaserController : MonoBehaviour
             Debug.Log("Distance is Safe now: " + targetBehaviour.fDistance + "m");
         }
 
+        // (파괴되는 등)시동이 꺼져서 기동이 불가능할 경우: 게임오버
         if (!helicopterInfo.bIsFlyable)
         {
             StopCoroutine("MissionTimer");
             GameOver();
         }
 
-        // DEVLOG 05-09:
+        // DEVLOG 05-09: 게임오버 UI
+        // 추격이 성공하였을 경우: 
         if (bAccomplished)
         {
             // 종료
@@ -115,6 +127,7 @@ public class MidAirChaserController : MonoBehaviour
                 UnityEditor.EditorApplication.isPlaying = false;
             }
         }
+        // 추격을 실패하였을 경우: 
         if (bIsGameOver)
         {
             // 재시작
@@ -127,12 +140,6 @@ public class MidAirChaserController : MonoBehaviour
             {
                 UnityEditor.EditorApplication.isPlaying = false;
             }
-        }
-
-        // 05-21: 추격을 시작합니다. 
-        if (!bChaseStart && targetBehaviour.fDistance <= fMaxDistance)
-        {
-            bChaseStart = true;
         }
     }
 
@@ -154,6 +161,10 @@ public class MidAirChaserController : MonoBehaviour
         objAccomplishedPanel.SetActive(true);
     }
 
+    /// <summary>
+    /// 플레이어가 타겟과 <code>fMaxDistance</code>만큼 떨어져 있을 경우 
+    /// 3초 카운트를 셀 때까지 돌아와야 하는데 그 카운트다운을 해주는 코루틴 함수입니다. 
+    /// </summary>
     IEnumerator MissingCountdown()
     {
         textMissingAlert.gameObject.SetActive(true);
@@ -177,14 +188,14 @@ public class MidAirChaserController : MonoBehaviour
     }
 
     /// <summary>
-    /// 미션을 실패하여 게임 오버가 되었을 경우 게임 오버를 화면에 출력하고 다시 시작 또는 종료를 유도합니다. 
+    /// 미션을 실패하여 게임 오버가 되었을 경우
+    /// 게임 오버를 화면에 출력하고 다시 시작 또는 종료를 유도합니다. 
     /// </summary>
     void GameOver()
     {
         if (!bIsGameOver)
         {
             objGameOverPanel.SetActive(true);
-
             bIsGameOver = true;
         }
     }
